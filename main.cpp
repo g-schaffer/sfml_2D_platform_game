0717e00a7f349234a2ec9582a700f8493014d808
Nicolas Vetroff <nv06270@outlook.fr>
2021-09-13 20:59:27 +0200

    Hello

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main() {
-	
+	sf::RenderWindow window(sf::VideoMode(900,600), "title");
+
+	while (window.isOpen()) {
+		sf::Event event;
+
+		if (event.Closed) {
+			window.close();
+		}
+	}
 }
