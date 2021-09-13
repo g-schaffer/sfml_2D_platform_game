9ec3b6ba8364690d2ec9e370909753a370b43b96
Nicolas Vetroff <nv06270@outlook.fr>
2021-09-13 21:19:43 +0200

    ordering the beginning

--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(900,600), "title");
-
-	while (window.isOpen()) {
-		sf::Event event;
-
-		if (event.Closed) {
-			window.close();
-		}
-	}
+	
 }
