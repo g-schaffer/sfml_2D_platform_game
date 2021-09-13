e452b3713703ef4354094a20c9b0d91959967ece
Nicolas Vetroff <nv06270@outlook.fr>
2021-09-13 20:33:46 +0200

    project started

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
-//CODE
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(900, 600), "My first Window");
+	while (window.isOpen()) {
+		sf::Event event;
+
+		if (event.type == sf::Event::Closed)
+			window.close();
+	}
+	window.display();
+}
