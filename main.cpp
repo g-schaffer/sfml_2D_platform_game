9df02f9d5d315e6b3fc0c174019c90ae570cb319
Nicolas Vetroff <nv06270@outlook.fr>
2021-09-13 20:44:53 +0200

    all from scratch

--- main.cpp
+++ main.cpp
@@ -2,12 +2,5 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(900, 600), "My first Window");
-	while (window.isOpen()) {
-		sf::Event event;
-
-		if (event.type == sf::Event::Closed)
-			window.close();
-	}
-	window.display();
+	
 }
